b15aaded1366ef2d0076c985aaf3bdba260aa9ca
Olli Liukkonen <Olli Liukkonen@ictlab.local>
2019-01-08 11:37:45 +0200

    initial push

--- Assets/scripts/player/Camera.cs
+++ Assets/scripts/player/Camera.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
+
+    private float x;
+    private float y;
+    private Vector3 rotateValue;
+
+    public Transform player;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +18,17 @@ public class Camera : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        if (Input.GetButton("Fire3")){
+
+            transform.RotateAround(Vector3.zero, Vector3.up, 20 * Time.deltaTime);
+            transform.LookAt(player);
+
+            //y = Input.GetAxis("Mouse X");
+            //x = Input.GetAxis("Mouse Y");
+            //rotateValue = new Vector3(x, y * -1, 0);
+            //transform.eulerAngles = transform.eulerAngles - rotateValue;
+        }
     }
 }
